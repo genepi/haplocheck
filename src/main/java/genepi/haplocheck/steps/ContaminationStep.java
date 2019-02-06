eb138307890baa4d74096f06d40b125b7a0d0e66
seppinho <sebastian.schoenherr@i-md.ac.at>
2019-02-06 22:42:05 +0100

    Add guava

--- src/main/java/genepi/haplocheck/steps/ContaminationStep.java
+++ src/main/java/genepi/haplocheck/steps/ContaminationStep.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
+import com.google.common.math.Quantiles;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
@@ -26,7 +27,6 @@ import importer.VcfImporter;
 import phylotree.Phylotree;
 import phylotree.PhylotreeManager;
 import util.ExportUtils;
-import com.google.common.math.Quantiles;
 import vcf.Sample;
 
 public class ContaminationStep extends WorkflowStep {
