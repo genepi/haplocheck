b0b82c09ea0678e843d80fc0c342d21a97090151
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2019-02-08 13:58:59 +0100

    Add missing library

---
+++ src/main/java/genepi/haplocheck/steps/contamination/objects/Font.java
@@ -0,0 +1,18 @@
+package genepi.haplocheck.steps.contamination.objects;
+
+public class Font {
+
+	private String color;
+
+	public Font(String color) {
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
