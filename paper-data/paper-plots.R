0252b15b9a2fe54e74e4800a60353cbadc432667
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2019-07-11 12:48:51 +0200

    Update paper plots

--- paper-data/paper-plots.R
+++ paper-data/paper-plots.R
@@ -1,18 +1,20 @@
 library("ggplot2")
 library("tidyverse")
 library("ggpubr")
+
 setwd("/data2/eclipse/mtdna/haplocheck/paper-data/");
 file = "simulated-data/output/simulation_3_All_500k_kulczynski.txt";
   simAll = read.table(file, header = TRUE, sep=",");
-simAllFilter = filter(simAll,simAll$HaplogrepFilter == 0.5  & simAll$Setup <= 7 & simAll$Group<=12);
+simAllFilter = filter(simAll,simAll$HaplogrepFilter == 0.5  & simAll$Setup <= 6 & simAll$Group<=8);
 simAllFilter$F1 = 2*(simAllFilter$Sensitivity * simAllFilter$Precision/(simAllFilter$Sensitivity + simAllFilter$Precision));
 p <- ggplot(simAllFilter, aes(x = Group, y = F1,colour=factor(Setup))) + 
   geom_line();
 p <- p + xlab('Variant Noise') +   ylab('F1 Score') + labs(colour = "Haplocheck Setup") + 
-  scale_x_continuous(breaks = c(0,4,8,12,16), labels = c("0", "4", "8", "12","16")) 
+  scale_x_continuous(breaks = c(0,4,8), labels = c("0", "4", "8")) + coord_cartesian(ylim=c(0.7,1))
 p <- p + scale_colour_brewer(palette = "Set1")
 all_final <- p + theme_light() +  theme(legend.position="top")
 
+all_final
 
 filterAllSetup = filter(simAll,simAll$HaplogrepFilter == 0.5  & simAll$Setup ==3);
 filterAllSetup$F1 = 2*(filterAllSetup$Sensitivity * filterAllSetup$Precision/(filterAllSetup$Sensitivity + filterAllSetup$Precision));
@@ -24,19 +26,18 @@ filterAllSetup
 
 file = "simulated-data/output/simulation_2_H_100K_kulczynski.txt";
 simH = read.table(file, header = TRUE, sep=",");
-simHFilter = filter(simH,simH$HaplogrepFilter == 0.5  & simH$Setup <= 7 & simH$Group<=8);
+simHFilter = filter(simH,simH$HaplogrepFilter == 0.5  & simH$Setup <= 6 & simH$Group<=8);
 simHFilter$F1 = 2*(simHFilter$Sensitivity * simHFilter$Precision/(simHFilter$Sensitivity + simHFilter$Precision));
 p <- ggplot(simHFilter, aes(x = Group, y = F1,colour=factor(Setup))) + 
   geom_line();
 p <- p + xlab('Variant Noise') +   ylab('F1 Score') + labs(colour = "Haplocheck Setup") + 
-  scale_x_continuous(breaks = c(0,4,8,12,16), labels = c("0", "4", "8", "12","16")) 
+  scale_x_continuous(breaks = c(0,4,8), labels = c("0", "4", "8")) + coord_cartesian(ylim=c(0.7,1))
 h_final <- p + scale_colour_brewer(palette = "Set1") + theme_light() +  theme(legend.position="top")
-
+h_final
 
 filterHSetup = filter(simH,simH$HaplogrepFilter == 0.5  & simH$Setup ==3);
 filterHSetup$F1 = 2*(filterHSetup$Sensitivity * filterHSetup$Precision/(filterHSetup$Sensitivity + filterHSetup$Precision));
 filterHSetup
-install.packages("ggpubr")
 filterHSetup = filter(simH,simH$HaplogrepFilter == 0.5  & simH$Setup ==4);
 filterHSetup$F1 = 2*(filterHSetup$Sensitivity * filterHSetup$Precision/(filterHSetup$Sensitivity + filterHSetup$Precision));
 filterHSetup
