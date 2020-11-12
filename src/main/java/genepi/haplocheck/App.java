3a4938bb9bbbe2c9560451e2aa9040cc04c6c307
seppinho <sebastian.schoenherr@i-med.ac.at>
2020-11-12 14:53:07 +0100

    Prepare release v1.3.2

--- src/main/java/genepi/haplocheck/App.java
+++ src/main/java/genepi/haplocheck/App.java
@@ -7,7 +7,7 @@ public class App {
 
 	public static final String APP = "haplocheck";
 
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.2";
 
 	public static final String URL = "https://github.com/genepi/haplocheck";
 
